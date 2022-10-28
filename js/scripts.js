8a8451467bd46405a24cdbdf3fa1233df70e0228
VW <34698193+QuietEvolver@users.noreply.github.com>
2022-10-28 15:19:20 -0700

    It will add commented out pseudocode.

--- js/scripts.js
+++ js/scripts.js
@@ -90,6 +90,22 @@ parseInt(inputValueOutOfRangeTest);
 
 
 
+// let inputArray = [1,2,3,4,5,6,7,8,9,0,1,2,5,6,7,8,]
+// const isValidCardNumber = isANumber(inputArray);
+// function isANumber(inputArray) {
+//   //if (inputNumber.length === 16 && typeOf(input) !== 'string'){
+//     // for(let i = -1; i <= inputNumber.length; i+= -1){
+//       if(inputArray.length === 16){
+//         console.log('input number = ', inputArray);
+//         console.log('inputnumber.length = ', inputArray.length);
+//         return true;
+//       // return inputNumber;
+//       } else {
+//         return false;
+//       }
+//     }
+//   console.log(isValidCardNumber);
+
 //PSEUDOCODE
 // let userInput = doc.value
 
@@ -100,6 +116,9 @@ parseInt(inputValueOutOfRangeTest);
 
 // weigh hierarchy 1-3
 // output stmts
+
+
+// TBA: hierarchy 
 
 let input = "";
 let message = "";
@@ -121,3 +140,10 @@ function numberNeighborhoodMessage(input){
     message = "Outside?"
   }
 } 
+
+// the test for mult  hierarchy dependent nums
+
+// if this number is 1 then boop
+// if this number is 1 <= 2 then boop 
+// if this number is 1 <= 2 then boop && if 2 <= currentNum:3
+// if number is not 1, 2, 3, then outside;
