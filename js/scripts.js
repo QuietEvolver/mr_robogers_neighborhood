5857122790dc1a171fd67c7d405938e6ac1bc0fd
VW <34698193+QuietEvolver@users.noreply.github.com>
2022-10-28 12:16:32 -0700

    It will add a switch case inputValueThreeTestFx.

--- js/scripts.js
+++ js/scripts.js
@@ -47,16 +47,19 @@ function inputValueNumberBoopFx2(inputValueNumberBoop2){
   }
 }
 
-let three = 3
+
+let inputThree = parseInt("3");
 let msg3 = "";
-switch(three) {
-  case 1:
-    msg3 = "Beep!"
-    break;
-  case 2:
-    msg3 = "Boop!"
-    break;
-  case 3:
-    msg3 = "Won't you be my neighbor?"
-    break;
+function inputValueThreeTestFx(inputThree){
+  switch(inputThree) {
+    case 1:
+      msg3 = "Beep!"
+      break;
+    case 2:
+      msg3 = "Boop!"
+      break;
+    case 3:
+      msg3 = "Won't you be my neighbor?"
+      break;
+  }
 }
