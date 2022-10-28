d59acadd8dcdd66430ed3fc06ca2a4ff63e9f506
VW <34698193+QuietEvolver@users.noreply.github.com>
2022-10-28 09:53:08 -0700

    It will pass second and third test.

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,12 @@ let input = "x"
 function inputValue(input){
   console.log("input value: ", input);
 }
+
+
+function inputValueNumber(inputValue){
+  if (0 >= inputValue <= 9){
+    console.log("This is a number: ", inputValue);
+  }
+  
+}
+
