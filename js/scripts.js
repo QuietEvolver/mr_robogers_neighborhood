b5533554c6145496a5c11dc4cec6a23b3d02a6d7
Vera Weikel <veraweikel@Veras-MacBook-Pro-3.local>
2022-11-07 19:17:42 -0800

    It will add the js for the first test.

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+// Buisiness Logic 
+
+function beepBoop(){
+  let input = [5, 1];
+ let countUpBy = [];
+ for( let i = 0; i <= input[0]; i += input[1]){
+   console.log(i);
+   countUpBy.push(i);
+ }
+}
