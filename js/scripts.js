1fb2f4da7618f562b7ee04d3da7aa86192da52fb
Vera Weikel <veraweikel@Veras-MacBook-Pro-3.local>
2022-11-08 17:34:24 -0800

    It will pass the second test.

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,16 @@
 // Buisiness Logic 
 
-function beepBoop(){
+(function beepBoop(){
   let input = [5, 1];
- let countUpBy = [];
- for( let i = 0; i <= input[0]; i += input[1]){
-   console.log(i);
-   countUpBy.push(i);
- }
-}
+  let countUpByIntake = [];
+  console.log("countUpByIntakeIntake",countUpByIntake); 
+  for( let i = 0; i <= input[0]; i += input[1]){
+      if (1 === i) {
+        message = "Beep!";
+      countUpByIntake.push(message);
+      } else{
+          countUpByIntake.push(i);
+        // turn i into a Str then push? no it comes out.
+      }  
+  }
+})();
