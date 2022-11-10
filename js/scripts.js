bea7e5848f32d9a4a969fd94f2ac88a0e7d3a98a
Vera Weikel <veraweikel@Veras-MacBook-Pro-3.local>
2022-11-10 02:11:41 -0800

    It will add js test for string split .

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,13 @@
 // Buisiness Logic 
 
-(function beepBoop(){
-  let input = [5, 1];
+function beepBoop(input) {
+  // let input = [12, 1];
   let countUpByIntake = [];
-  console.log("countUpByIntakeIntake",countUpByIntake); 
-  for( let i = 0; i <= input[0]; i += input[1]){
-      if (1 === i) {
-        message = "Beep!";
-      countUpByIntake.push(message);
-      } else{
-          countUpByIntake.push(i);
-        // turn i into a Str then push? no it comes out.
-      }  
+  console.log("countUpByIntake ", countUpByIntake);
+  for (let i = 0; i <= input[0]; i += input[1]) { 
+    countUpByIntake.push(i);
   }
-})();
+  let cubiSplitSTR = countUpByIntake.toString();
+  let cubiSplit = cubiSplitSTR.split(",");
+  return cubiSplit;
+}
